489e39260ff320d84e86ef3af4265e7e313fa2ec
pe.arnold <75440663+arnoldpe84961@users.noreply.github.com>
2025-10-27 19:35:26 +0100

    Assesment/1 website setup (#2)
    * add mandatory sections
    
    * setup vue
    
    * add github action workflow for deployment

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # vizis
+
+## Vue configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/components/Section1.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <div class="container section__inner">
+      <h2>Visualizing Conflict and Human Suffering through Data</h2>
+      <p>
+        This is a placeholder for the first section. In the future we will present one section for each assessment task.
+      </p>
+      <p>The design may still be adjusted, as this only applies to the first review.</p>
+      <p>The goal of this assesment task is to set up our GitHub page and show a first section.</p>
+    </div>
+  </section>
+</template>
