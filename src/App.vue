128b4e11b7d1ba61cc75a62a35b4b8fcbb25482f
pe.arnold <pe.arnold@outlook.com>
2025-10-27 19:25:21 +0100

    setup vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Section1 from './components/Section1.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Data Visualization</h1>
+    <div class="subtitle">
+      <h2>Vizis</h2>
+      <h4>Maleah Leonie Koegler, Kim Benecken, Matteo Glaeser, Victoria Cruz, Petula Arnold</h4>
+    </div>
+  </header>
+
+  <main>
+    <Section1 />
+  </main>
+</template>
